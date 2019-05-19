c999b0e72fdc86e7ba185ca76082e5f9531c7cdf
Hatice Özdilek <48315417+haticeozdilek@users.noreply.github.com>
2019-05-19 18:38:42 +0300

    Rename yapısal programlama final-4.soru.c to yapısal programlama final-3.soru.c

--- yapısal programlama final-3.soru.c
+++ yapısal programlama final-3.soru.c
@@ -5,15 +5,15 @@ int main(){
     int a=1,b=2,c=3;
     int *ptr1,*ptr2,*ptr3;
 
-    ptr1=&a; //ptr1 a'nın adresini tutuyor
+    ptr1=&a; //ptr1 a'nÄ±n adresini tutuyor
     ptr2=&b;  //ptr2 b'nin adresini tutuyor.
     ptr3=&c;  //ptr3 c'nin adresini tutuyor
 
     *ptr1=*ptr2 +*ptr3;  ///->    a=b+c'den a=5 oluyor
-    *ptr2=*ptr1+6;       ///->    b=a+6'dan b=11 oluyor(a 'nın değeri 5 idi)
-    ptr3=ptr1;           ///->    c=a c'nin değeri a'ya atanıyor
+    *ptr2=*ptr1+6;       ///->    b=a+6'dan b=11 oluyor(a 'nÄ±n deÄŸeri 5 idi)
+    ptr3=ptr1;           ///->    c=a c'nin deÄŸeri a'ya atanÄ±yor
     *ptr1=*ptr2 + *ptr3;///->     a=b+c=5+11 den a=16 oluyor
-    printf("a=%d,b=%d,c=%d\n",a,b,c); ///c'nin değeri değişmeden aynen kaldı
+    printf("a=%d,b=%d,c=%d\n",a,b,c); ///c'nin deÄŸeri deÄŸiÅŸmeden aynen kaldÄ±
 }
 ///a=16
 ///b=11
